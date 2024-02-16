47e4632e7519cceef4283d53f5620c5fd36acc41
olive.koube <olive.koube@inexa-ci.com>
2024-02-16 15:14:49 +0000

    Ajou projet Infrastructure

--- src/Bookify.Application/Abstractions/Behaviors/LoggingBehaviors.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Bookify.Application.Abstractions.Behaviors;
-
-public class LoggingBehaviors
-{
-    
-}
--- src/Bookify.Infrastructure/obj/Debug/net8.0/Bookify.Infrastructure.AssemblyInfo.cs
+++ src/Bookify.Infrastructure/obj/Debug/net8.0/Bookify.Infrastructure.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Bookify.Domain")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Bookify.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Bookify.Domain")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Bookify.Domain")]
+[assembly: System.Reflection.AssemblyProductAttribute("Bookify.Infrastructure")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Bookify.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
